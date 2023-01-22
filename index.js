3c0b3b387d070ace36eab22d2ac100f1c78f2de8
githumbi <githumbi74@gmail.com>
2023-01-22 11:46:32 +0300

    adding toolkit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/iceCream/iceCreamSlice').icecreamActions
 
 console.log('initial state', store.getState())
 
@@ -11,6 +12,10 @@ const unsubscribe = store.subscribe(() => {
  store.dispatch(cakeActions.ordered())
  store.dispatch(cakeActions.ordered())
  store.dispatch(cakeActions.restocked(3))
+ store.dispatch(icecreamActions.ordered())
+ store.dispatch(icecreamActions.ordered())
+ store.dispatch(icecreamActions.ordered())
+ store.dispatch(icecreamActions.restocked(4))
 
  unsubscribe()
 
