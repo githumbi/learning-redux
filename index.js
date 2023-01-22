8701170af89b93c8afd30ec09efaf4924ca67cc5
githumbi <githumbi74@gmail.com>
2023-01-22 13:08:30 +0300

    async thunk

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
   require("./features/iceCream/iceCreamSlice").icecreamActions;
+const fetchUsers = require("./features/users/userSlice").fetchUsers
 
-console.log("initial state", store.getState());
+// console.log("initial state", store.getState());
 
 const unsubscribe = store.subscribe(() => {});
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(4));
+store.dispatch(fetchUsers());
 
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(4));
+
+// unsubscribe();
