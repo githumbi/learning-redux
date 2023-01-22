06dddced35909c94a1319178167f5dbf4a6c6f88
githumbi <githumbi74@gmail.com>
2023-01-22 11:58:40 +0300

    adding logger middle ware

--- app/store.js
+++ app/store.js
@@ -1,12 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
-const icecreamReducer = require('../features/iceCream/iceCreamSlice')
+const icecreamReducer = require("../features/iceCream/iceCreamSlice");
+const reduxLogger = require("redux-logger");
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const store = require('./app/store')
-const cakeActions = require('./features/cake/cakeSlice').cakeActions
-const icecreamActions = require('./features/iceCream/iceCreamSlice').icecreamActions
+const store = require("./app/store");
+const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+  require("./features/iceCream/iceCreamSlice").icecreamActions;
 
-console.log('initial state', store.getState())
+console.log("initial state", store.getState());
 
-const unsubscribe = store.subscribe(() => { 
-  console.log('updated state', store.getState())
- })
+const unsubscribe = store.subscribe(() => {});
 
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.restocked(3))
- store.dispatch(icecreamActions.ordered())
- store.dispatch(icecreamActions.ordered())
- store.dispatch(icecreamActions.ordered())
- store.dispatch(icecreamActions.restocked(4))
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(4));
 
- unsubscribe()
-
- 
+unsubscribe();
